cc681c5c54ba50950033edc12bf4c4e503c84ff8
Dan G <freemvmt@posteo.net>
2025-06-12 15:54:40 +0100

    setup.sh: source uv after install

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@ apt update
 # ensure we have all the utils we need
 apt install -y vim rsync git
 apt upgrade -y
-# install uv and syn
+# install uv and sync
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 uv sync
 # activate virtual environment for running python scripts
 source .venv/bin/activate
