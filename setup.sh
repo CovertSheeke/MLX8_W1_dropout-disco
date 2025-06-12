f0a7bb3d3315afb03677b9cbdfe00e54a56ebdc6
Dan G <freemvmt@posteo.net>
2025-06-12 16:07:29 +0100

    more apt pkg for setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# run like `source setup.sh` to ensure active shell is set up
+# run like `source setup.sh` to ensure active shell is set up with venv
 apt update
 # ensure we have all the utils we need
-apt install -y vim rsync git
+apt install -y vim rsync git nvtop htop tmux curl
 apt upgrade -y
 # install uv and sync
 curl -LsSf https://astral.sh/uv/install.sh | sh
