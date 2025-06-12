1405f0ec4250b511c21d6cea8e56c193a6e50228
Dan G <freemvmt@posteo.net>
2025-06-12 16:04:44 +0100

    one final script to push .env to remote

--- pull_weights.sh
+++ pull_weights.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-# assumes setup.sh was run previously, so that remote has rsync
+# assumes ssh config for remote is set up as 'mlx'
+# also assumes setup.sh was run previously, so that remote has rsync
 rsync -avz --exclude='checkpoint_*' mlx:MLX8_W1_dropout-disco/weights/ weights/
---
+++ push_env.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# assumes ssh config for remote is set up as 'mlx'
+scp .env mlx:MLX8_W1_dropout-disco/.env 
