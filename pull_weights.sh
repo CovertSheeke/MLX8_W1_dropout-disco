019d53846fe93143d50f1f30c6741d2d8d4242ee
Dan G <freemvmt@posteo.net>
2025-06-13 10:31:50 +0100

    improve pull_weights script, commit weights from latest run (vibrant-water)

--- pull_weights.sh
+++ pull_weights.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 # assumes ssh config for remote is set up as 'mlx'
 # also assumes setup.sh was run previously, so that remote has rsync
-rsync -avz --exclude='checkpoint_*' mlx:MLX8_W1_dropout-disco/weights/ weights/
+if [[ -z "${1-}" ]]; then
+    REMOTE="mlx"
+else
+    REMOTE="$1"
+fi
+rsync -avz --exclude='checkpoint_*' "$REMOTE:MLX8_W1_dropout-disco/weights/" weights/
