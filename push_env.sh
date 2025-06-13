b40ad15e0a73b8faa0b3621024b69944415c1a49
Dan G <freemvmt@posteo.net>
2025-06-13 14:11:03 +0100

    improve push_env.sh

--- push_env.sh
+++ push_env.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 # assumes ssh config for remote is set up as 'mlx'
-scp .env mlx:MLX8_W1_dropout-disco/.env 
+if [[ -z "${1-}" ]]; then
+    REMOTE="mlx"
+else
+    REMOTE="$1"
+fi
+scp .env "$REMOTE:MLX8_W1_dropout-disco/.env" 
