bdcf63b7cab322794d6519ff2b0a469f1bdb14e5
Dan G <freemvmt@posteo.net>
2025-06-13 14:21:52 +0100

    add git-lfs to setup.sh script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # run like `source setup.sh` to ensure active shell is set up with venv
 apt update
 # ensure we have all the utils we need
-apt install -y vim rsync git nvtop htop tmux curl
+apt install -y vim rsync git git-lfs nvtop htop tmux curl
 # apt upgrade -y
 # install uv and sync
 curl -LsSf https://astral.sh/uv/install.sh | sh
@@ -10,3 +10,5 @@ source $HOME/.local/bin/env
 uv sync
 # activate virtual environment for running python scripts
 source .venv/bin/activate
+echo "Setup complete - virtual environment activated. You can now run Python scripts directly."
+echo "Run 'git lfs pull' to download large files."
